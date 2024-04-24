30fc2cb40336c9e1187b16b61aeed2fbfce11a7f
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-04-24 14:43:55 +1000

    main: Add image loading and unsharp mask

--- pyarcfire/__main__.py
+++ pyarcfire/__main__.py
@@ -3,6 +3,12 @@ import argparse
 import logging
 from typing import Sequence
 
+# External libraries
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+from skimage import filters
+
 # Internal libraries
 from .log_utils import setup_logging
 
@@ -11,7 +17,31 @@ log = logging.getLogger(__name__)
 
 def main(raw_args: Sequence[str]) -> None:
     args = _parse_args(raw_args)
-    log.debug(args)
+    # Load image
+    image = np.asarray(Image.open(args.input_path).convert("L"))
+
+    UNSHARP_MASK_RADIUS: float = 25
+    UNSHARP_MASK_AMOUNT: float = 6
+    contrast_image = filters.unsharp_mask(
+        image, radius=UNSHARP_MASK_RADIUS, amount=UNSHARP_MASK_AMOUNT
+    )
+
+    fig = plt.figure()
+    left_axis = fig.add_subplot(121)
+    left_axis.imshow(image, cmap="gray")
+    left_axis.set_title("Original image")
+    left_axis.set_axis_off()
+
+    right_axis = fig.add_subplot(122)
+    right_axis.imshow(contrast_image, cmap="gray")
+    right_axis.set_title(
+        rf"Unsharp image $\text{{Radius}} = {UNSHARP_MASK_RADIUS}, \; \text{{Amount}} = {UNSHARP_MASK_AMOUNT}$"
+    )
+    right_axis.set_axis_off()
+
+    fig.tight_layout()
+    plt.show()
+    plt.close()
 
 
 def _parse_args(args: Sequence[str]) -> argparse.Namespace:
