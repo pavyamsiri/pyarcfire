4f47d2d4f556badd6b87bc1b37701db642c3d961
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-06-13 00:35:17 +1000

    merge: Fix lints

--- pyarcfire/merge.py
+++ pyarcfire/merge.py
@@ -1,22 +1,26 @@
-"""This module contains functions relating to calculating merging errors."""
+"""Functions relating to calculating merging errors."""
+
+from __future__ import annotations
 
 import logging
-import numpy as np
-from numpy.typing import NDArray
+from typing import TYPE_CHECKING, TypeVar
 
 from .arc import fit_spiral_to_image
 
 log: logging.Logger = logging.getLogger(__name__)
 
-FloatType = np.float32
+if TYPE_CHECKING:
+    import numpy as np
+    from numpy.typing import NDArray
+
+    FloatType = TypeVar("FloatType", np.float32, np.float64)
 
 
-def calculate_arc_merge_error(
-    first_cluster_array: NDArray[FloatType], second_cluster_array: NDArray[FloatType]
-) -> float:
-    """Calculates the arc merge error ratio for two clusters. This is a measure of how
-    well the merged cluster of the two clusters given fit to a log spiral compared to
-    fitting the two clusters separately.
+def calculate_arc_merge_error(first_cluster_array: NDArray[FloatType], second_cluster_array: NDArray[FloatType]) -> float:
+    """Calculate the arc merge error ratio for two clusters.
+
+    This is a measure of how well the merged cluster of the two clusters given fit to a log spiral
+    compared to fitting the two clusters separately.
 
     Parameters
     ----------
@@ -29,13 +33,14 @@ def calculate_arc_merge_error(
     -------
     merge_error_ratio : float
         The arc merge error ratio.
-    """
 
+    """
     first_sum = first_cluster_array.sum()
     second_sum = second_cluster_array.sum()
     # Verify we don't have division by zero or weird negative weights
     if first_sum <= 0 or second_sum <= 0:
-        raise ValueError("The cluster arrays must sum to a positive value.")
+        msg = "The cluster arrays must sum to a positive value."
+        raise ValueError(msg)
     total_sum = first_sum + second_sum
     # Adjust weights
     first_cluster_array *= total_sum / first_sum
@@ -55,11 +60,8 @@ def calculate_arc_merge_error(
         combined_cluster_array,
         initial_pitch_angle=second_fit.pitch_angle,
     )
-    if first_merged_fit.total_error <= second_merged_fit.total_error:
-        merged_fit = first_merged_fit
-    else:
-        merged_fit = second_merged_fit
-
+    first_fit_is_better = first_merged_fit.total_error <= second_merged_fit.total_error
+    merged_fit = first_merged_fit if first_fit_is_better else second_merged_fit
     first_cluster_indices = (first_cluster_array > 0)[combined_cluster_array > 0]
     # Get the error of the merged spiral for each individual cluster
     first_cluster_errors = merged_fit.errors[first_cluster_indices].sum()
