4d1db851ea10f4f4cf04f1d7152f26c35b9dea96
Pavadol Yamsiri <pyam4330@uni.sydney.edu.au>
2024-06-14 15:12:21 +1000

    assert_utils: Functions to verify data input.
    First function is to verify data normalization.

---
+++ pyarcfire/assert_utils.py
@@ -0,0 +1,17 @@
+"""Functions to verify inputs."""
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from numpy.typing import NDArray
+
+
+def verify_data_is_normalized(data: NDArray[Any]) -> None:
+    not_normalized = np.min(data) < 0 or np.max(data) > 1
+    if not_normalized:
+        msg = "The data is not normalized to the range [0, 1]! Please normalize your data before using this function."
+        raise ValueError(msg)
