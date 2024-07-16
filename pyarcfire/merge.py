f9506006147001cf2092474bd0dc0f87fa779941
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-07-16 16:54:53 +1000

    merge: Fix calculate_arc_merge_error
    We were summing residuals instead of square residuals.

--- pyarcfire/merge.py
+++ pyarcfire/merge.py
@@ -5,12 +5,13 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING, TypeVar
 
+import numpy as np
+
 from .arc import fit_spiral_to_image
 
 log: logging.Logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
-    import numpy as np
     from numpy.typing import NDArray
 
     FloatType = TypeVar("FloatType", np.float32, np.float64)
@@ -53,8 +54,8 @@ def calculate_arc_merge_error(
     first_fit = fit_spiral_to_image(first_reweighted_array)
     second_fit = fit_spiral_to_image(second_reweighted_array)
 
-    combined_cluster_array = first_reweighted_array + second_reweighted_array
     # Fit a spiral to both clusters at the same time
+    combined_cluster_array = first_reweighted_array + second_reweighted_array
     first_merged_fit = fit_spiral_to_image(
         combined_cluster_array,
         initial_growth_factor=first_fit.growth_factor,
@@ -66,18 +67,14 @@ def calculate_arc_merge_error(
     first_fit_is_better = first_merged_fit.total_error <= second_merged_fit.total_error
     merged_fit = first_merged_fit if first_fit_is_better else second_merged_fit
     first_cluster_indices = (first_reweighted_array > 0)[combined_cluster_array > 0]
-    # Get the error of the merged spiral for each individual cluster
-    first_cluster_errors = merged_fit.errors[first_cluster_indices].sum()
-    second_cluster_errors = merged_fit.errors[~first_cluster_indices].sum()
-
-    # Readjust errors from normalised cluster arrays
-    first_cluster_error_weighted = first_fit.total_error / first_sum
-    second_cluster_error_weighted = second_fit.total_error / second_sum
+    # Get the sse of the merged spiral for each individual cluster
+    first_cluster_error = np.square(merged_fit.errors[first_cluster_indices]).sum()
+    second_cluster_error = np.square(merged_fit.errors[~first_cluster_indices]).sum()
 
     # Calculate the arc merge error ratio for each cluster
     ratios = (
-        ((first_cluster_errors / first_sum) / first_cluster_error_weighted),
-        ((second_cluster_errors / second_sum) / second_cluster_error_weighted),
+        (first_cluster_error / first_fit.total_error),
+        (second_cluster_error / second_fit.total_error),
     )
     # Return the worst error ratio
     return max(ratios)
