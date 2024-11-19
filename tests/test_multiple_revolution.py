4d85a4afa6de217b5719b807947d82dbbd6bdcf7
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-11-19 12:38:13 +1100

    tests: Improve type stubs

--- tests/test_multiple_revolution.py
+++ tests/test_multiple_revolution.py
@@ -1,13 +1,20 @@
 """Tests for the arc module, specifically regarding how multiple revolution spirals are fitted."""
 
+from typing import TypeVar
+
 import numpy as np
 from hypothesis import given
 from hypothesis import strategies as st
 from hypothesis.strategies._internal.core import DrawFn
 from numpy import testing as nptesting
-from numpy.typing import NDArray
 
-from pyarcfire.arc.fit import _find_single_revolution_regions_polar
+from pyarcfire.arc.fit import _find_single_revolution_regions_polar  # pyright:ignore[reportPrivateUsage]
+
+_SCT = TypeVar("_SCT", bound=np.generic)
+_Array1D = np.ndarray[tuple[int], np.dtype[_SCT]]
+_Array2D = np.ndarray[tuple[int, int], np.dtype[_SCT]]
+_Array1D_bool = _Array1D[np.bool_]
+_Array2D_bool = _Array2D[np.bool_]
 
 
 @st.composite
@@ -16,7 +23,7 @@ def valid_polar_images(
     min_size: int,
     max_size: int,
     shrink_amount: int,
-) -> tuple[NDArray[np.bool_], NDArray[np.bool_], int]:
+) -> tuple[_Array2D_bool, _Array1D_bool, int]:
     """Generate a valid test polar image.
 
     A polar image is an image transformed to be in the polar coordinate frame.
@@ -34,9 +41,9 @@ def valid_polar_images(
 
     Returns
     -------
-    arr : NDArray[np.bool_]
+    arr : Array2D[bool]
         The polar image as a binary array where `True` means it is part of the cluster.
-    expected : NDArray[np.bool_]
+    expected : Array1D[bool]
         The polar angle bins where the cluster does only a single revolution.
     shrink_amount: int
         The amount to shrink the cluster by in polar angle bin units.
@@ -47,7 +54,7 @@ def valid_polar_images(
     single_region_size: int = draw(st.integers(shrink_amount, size))
     shift_amount: int = draw(st.integers(0, size - 1))
 
-    arr: NDArray[np.bool_] = np.zeros((4, size), dtype=np.bool_)
+    arr: _Array2D_bool = np.zeros((4, size), dtype=np.bool_)
     arr[0, :double_region_size] = True
     arr[2, :single_region_size] = True
     arr = np.roll(arr, shift_amount, axis=1)
@@ -66,17 +73,17 @@ def valid_polar_images(
 
 @given(valid_polar_images(min_size=5, max_size=360, shrink_amount=5))
 def test_find_single_revolution_regions_polar(
-    data: tuple[NDArray[np.bool_], NDArray[np.bool_], int],
+    data: tuple[_Array2D_bool, _Array1D_bool, int],
 ) -> None:
     """Test the function which identifies single revolution polar angle bins.
 
     Parameters
     ----------
-    data : tuple[NDArray[np.bool_], NDArray[np.bool_], int]
+    data : tuple[Array2D[bool], Array1D[bool], int]
         This tuple is comprised of three components.
-    arr : NDArray[np.bool_]
+    arr : Array2D[bool]
         The polar image as a binary array where `True` means it is part of the cluster.
-    expected : NDArray[np.bool_]
+    expected : Array1D[bool]
         The polar angle bins where the cluster does only a single revolution.
     shrink_amount: int
         The amount to shrink the cluster by in polar angle bin units.
