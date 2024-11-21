7733b92942c9b80fd4ec4bd242476aafd5d314b1
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-11-21 15:36:22 +1100

    REFACTOR: Remove `spiral.py` (the old API) and export preprocessors

--- src/pyarcfire/__init__.py
+++ src/pyarcfire/__init__.py
@@ -6,29 +6,30 @@ the corresponding [paper](https://ui.adsabs.harvard.edu/abs/2014ApJ...790...87D/
 
 import importlib.metadata
 
+from . import preprocess
 from .arc import LogSpiralFitResult, fit_spiral_to_image, log_spiral
 from .cluster import GenerateClustersSettings, generate_clusters
+from .finder import SpiralFinder, SpiralFinderResult
 from .merge_fit import MergeClustersByFitSettings, merge_clusters_by_fit
 from .orientation import GenerateOrientationFieldSettings, generate_orientation_fields
 from .similarity import GenerateSimilarityMatrixSettings, generate_similarity_matrix
-from .spiral import ClusterSpiralResult, UnsharpMaskSettings, detect_spirals_in_image
 
 __version__ = importlib.metadata.version(__name__)
 
 
 __all__ = [
-    "ClusterSpiralResult",
-    "GenerateClustersSettings",
-    "GenerateOrientationFieldSettings",
-    "GenerateSimilarityMatrixSettings",
-    "LogSpiralFitResult",
-    "MergeClustersByFitSettings",
-    "UnsharpMaskSettings",
-    "detect_spirals_in_image",
     "fit_spiral_to_image",
     "generate_clusters",
+    "GenerateClustersSettings",
     "generate_orientation_fields",
+    "GenerateOrientationFieldSettings",
     "generate_similarity_matrix",
+    "GenerateSimilarityMatrixSettings",
     "log_spiral",
+    "LogSpiralFitResult",
     "merge_clusters_by_fit",
+    "MergeClustersByFitSettings",
+    "preprocess",
+    "SpiralFinder",
+    "SpiralFinderResult",
 ]
